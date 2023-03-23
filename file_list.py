c44554549d62ff24e9f271db2a1586eba2b577d8
Deocomate <deocomate@GMAIL.COM>
2023-03-24 00:25:55 +0700

    Update

--- file_list.py
+++ file_list.py
@@ -6,7 +6,7 @@ html_files = []
 for filename in os.listdir("."):
     # Nếu tệp có đuôi .html thì thêm vào danh sách
     if filename.endswith(".html"):
-        html_files.append(f"<a href='/{filename}' style='display: block'>{filename}</a>")
+        html_files.append(f"<a href='./{filename}' style='display: block'>{filename}</a>")
 # In danh sách các tệp có đuôi .html
 print(html_files)
 
