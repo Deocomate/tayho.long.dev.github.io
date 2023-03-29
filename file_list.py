088c872f3921d0db5f1d9eb469be0447ffa58269
Deocomate <deocomate@GMAIL.COM>
2023-03-29 13:58:08 +0700

    OKOK

--- file_list.py
+++ file_list.py
@@ -3,10 +3,10 @@ import os
 html_files = []
 
 # Lặp qua các tệp trong thư mục hiện tại
-for filename in os.listdir("."):
+for filename in os.listdir("./long-page"):
     # Nếu tệp có đuôi .html thì thêm vào danh sách
     if filename.endswith(".html"):
-        html_files.append(f"<a href='./{filename}' style='display: block'>{filename}</a>")
+        html_files.append(f"<a href='./long-page/{filename}' style='display: block'>{filename}</a>")
 # In danh sách các tệp có đuôi .html
 print(html_files)
 
